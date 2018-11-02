9ed07a777b32720a41b0f7314735cf2326b03b10
Vladimir Kleshko <uside.node@gmail.com>
2018-11-02 17:46:18 +0200

    Add eslint and error logging

--- providers/BackupProvider.js
+++ providers/BackupProvider.js
@@ -1,15 +1,15 @@
-"use strict";
+'use strict'
 
-const { ServiceProvider } = require("@adonisjs/fold");
+const { ServiceProvider } = require('@adonisjs/fold')
 
 class BackupProvider extends ServiceProvider {
-  register() {
-    this.app.singleton("Backup", () => {
-      const Helpers = this.app.use("Adonis/Src/Helpers");
-      const Config = this.app.use("Adonis/Src/Config");
-      return new (require("../src/Backup"))(Helpers, Config);
-    });
+  register () {
+    this.app.singleton('Backup', () => {
+      const Helpers = this.app.use('Adonis/Src/Helpers')
+      const Config = this.app.use('Adonis/Src/Config')
+      return new (require('../src/Backup'))(Helpers, Config)
+    })
   }
 }
 
-module.exports = BackupProvider;
+module.exports = BackupProvider
--- src/Backup.js
+++ src/Backup.js
@@ -1,78 +1,81 @@
-"use strict";
+'use strict'
 
-const path = require("path");
-const { spawn } = require("child_process");
-const exec = require("util").promisify(require("child_process").exec);
-const moment = require("moment");
+const path = require('path')
+const { spawn } = require('child_process')
+const exec = require('util').promisify(require('child_process').exec)
+const moment = require('moment')
 
 class Backup {
-  constructor(Helpers, Config) {
-    this.Helpers = Helpers;
-    this.Config = Config;
+  constructor (Helpers, Config) {
+    this.Helpers = Helpers
+    this.Config = Config
   }
 
-  exec(command, args) {
+  exec (command, args) {
     return new Promise(resolve => {
-      spawn(command, args).on("close", code => resolve(code));
-    });
+      let proc = spawn(command, args)
+      proc.stderr.on('data', console.error.bind(console))
+      proc.on('close', code => resolve(code))
+    })
   }
 
-  async perform() {
-    let filename = `${moment().format("YYYY-MM-DD_HH-m")}.backup`;
-    let filepath = path.join(this.Helpers.appRoot(), "tmp", filename);
+  async perform () {
+    let filename = `${moment().format('YYYY-MM-DD_HH-m')}.backup`
+    let filepath = path.join(this.Helpers.appRoot(), 'tmp', filename)
 
     if (
-      (await this.exec("pg_dump", [
-        this.Config.get("backup.url"),
-        "-f",
+      (await this.exec('pg_dump', [
+        this.Config.get('backup.url'),
+        '-Fc',
+        '-f',
         filepath
-      ])) != 0
+      ])) !== 0
     ) {
-      throw new Error("Dump failed!");
+      throw new Error('Dump failed!')
     }
 
     if (
-      (await this.exec("openssl", [
-        "aes-256-cbc",
-        "-e",
-        "-a",
-        "-salt",
-        "-pass",
-        `pass:${this.Config.get("backup.key")}`,
-        "-in",
+      (await this.exec('openssl', [
+        'aes-256-cbc',
+        '-e',
+        '-a',
+        '-salt',
+        '-pass',
+        `pass:${this.Config.get('backup.key')}`,
+        '-in',
         filepath,
-        "-out",
+        '-out',
         `${filepath}.enc`
-      ])) != 0
+      ])) !== 0
     ) {
-      throw new Error("Encryption failed!");
+      throw new Error('Encryption failed!')
     }
 
     if (
-      (await this.exec("b2", [
-        "authorize-account",
-        this.Config.get("backup.account"),
-        this.Config.get("backup.token")
-      ])) != 0
+      (await this.exec('b2', [
+        'authorize-account',
+        this.Config.get('backup.account'),
+        this.Config.get('backup.token')
+      ])) !== 0
     ) {
-      throw new Error("Authorization failed!");
+      throw new Error('Authorization failed!')
     }
 
-    let { stdout } = await exec(`sha1sum ${filepath}`);
+    let { stdout } = await exec(`sha1sum ${filepath}`)
     if (
-      (await this.exec("b2", [
-        "upload-file",
-        "--sha1",
-        stdout.split(" ")[0],
-        "--noProgress",
-        this.Config.get("backup.bucket"),
+      (await this.exec('b2', [
+        'upload-file',
+        '--sha1',
+        stdout.split(' ')[0],
+        '--noProgress',
+        this.Config.get('backup.bucket'),
         `${filepath}.enc`,
         filename
-      ])) != 0
+      ])) !== 0
     ) {
-      throw new Error("Upload failed!");
+      throw new Error('Upload failed!')
     }
   }
 }
 
-module.exports = Backup;
+module.exports = Backup
